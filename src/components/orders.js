6508fbdaefc7146f350b95286ab5612a92594d42
Michael McKay <mamckay89@gmail.com>
2019-01-10 19:53:51 -0700

    More deployment

--- src/components/orders.js
+++ src/components/orders.js
@@ -4,13 +4,18 @@ import './orders.css';
 export default function Orders(props) {
     console.log(props.orders);
     const list = props.orders.map((order,index) => {
-        return <li className='order-list' key={index}><span>{order.completed}</span>
+        if(order.shipdate){
+            return <li className='order-list' key={index}><span>{order.completed}</span>
         <span>{order.items[0]}</span>
         <br></br>
         <span>{order.address}</span>
         <br></br>
         <span>Delivery date: {order.shipdate.split('T')[0]}</span>
         </li>
+        }else{
+            return <span>No Orders</span>
+        }
+        
     })
     return <div><div className ='customHr'></div>{list}</div>
 }
